abc51a02d32870723c1c3274e16299e5f9c1ec0c
Anupam Chatterjee <98115888+anu9am@users.noreply.github.com>
2026-06-30 17:40:34 +0530

    Update CREATEInternshipPage.tsx

--- src/components/CREATEInternshipPage.tsx
+++ src/components/CREATEInternshipPage.tsx
@@ -22,7 +22,7 @@ function SectionBadge({ label }: { label: string }) {
 
 export default function CREATEInternshipPage() {
   return (
-    <main className="min-h-dvh flex flex-col gap-8 relative max-w-2xl mx-auto py-12 sm:py-24 px-6">
+    <main className="min-h-dvh flex flex-col gap-14 relative">
 
       {/* BACKGROUND "INTERNSHIP" TEXT */}
       <h1 className="text-[50px] sm:text-[90px] md:text-[120px] font-semibold font-mono bg-linear-to-b from-primary/30 to-secondary/10 text-transparent bg-clip-text absolute -top-10 md:-top-24 left-1/2 -translate-x-1/2 mask-[linear-gradient(to_bottom,black,black_30%,transparent_100%)] tracking-tighter uppercase [-webkit-text-stroke:2px_hsl(var(--primary)/0.6)] md:[-webkit-text-stroke:3px_hsl(var(--primary)/0.6)] whitespace-nowrap pointer-events-none select-none -z-10">
